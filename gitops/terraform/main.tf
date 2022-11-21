a3e45cf4fed2f43ceb259768abb955b6e5671b60
petervanek123 <118302675+petervanek123@users.noreply.github.com>
2022-11-21 10:20:45 +0100

    chg terraform "azurerm_role_assignment"

--- gitops/terraform/main.tf
+++ gitops/terraform/main.tf
@@ -52,9 +52,10 @@ resource "azurerm_kubernetes_cluster" "aks" {
     Name = "${var.project}-aks"
   }
 }
+data "azurerm_client_config" "principal" {}
 
 resource "azurerm_role_assignment" "role_assignment" {
-  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  principal_id                     = azurerm_client_config.principal.object_id
   role_definition_name             = "AcrPull"
   scope                            = azurerm_container_registry.container_registry.id
   skip_service_principal_aad_check = true
