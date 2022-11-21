df78a8b7e2cf6913375411d1e384f013edf4d6b7
petervanek123 <118302675+petervanek123@users.noreply.github.com>
2022-11-21 21:31:45 +0100

    uncomment terraform azurerm_role_assignment

--- gitops/terraform/main.tf
+++ gitops/terraform/main.tf
@@ -54,10 +54,10 @@ resource "azurerm_kubernetes_cluster" "aks" {
 }
 
 
-#resource "azurerm_role_assignment" "role_assignment" {
-#  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
-#  role_definition_name             = "AcrPull"
-#  scope                            = azurerm_container_registry.container_registry.id
-#  skip_service_principal_aad_check = true
-#}
+resource "azurerm_role_assignment" "role_assignment" {
+  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  role_definition_name             = "AcrPull"
+  scope                            = azurerm_container_registry.container_registry.id
+  skip_service_principal_aad_check = true
+}
 
